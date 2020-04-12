689aa1a73229d0cf1ac8cf81c4c0c058600815d7
jazzyk <jkirbyy@icloud.com>
2020-04-13 00:43:18 +0100

    created home page, added music player to interviews page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
-def index():
-    return "Hello World!!!"
+def home():
+    return render_template('home.html')
+
+@app.route('/interviews')
+def interviews():
+    return render_template('interviews.html')
